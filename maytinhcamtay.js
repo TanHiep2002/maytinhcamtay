2e927597cf7fd0405d8711b8e2723d4d4a82fd8b
TanHiep2002 <105424122+TanHiep2002@users.noreply.github.com>
2022-05-12 16:35:14 +0700

    Add files via upload

---
+++ maytinhcamtay.js
@@ -0,0 +1,21 @@
+var input = document.querySelector('.input');    
+var items = Array.from(document.querySelectorAll('.item'));
+items.forEach(function(bnt) {
+    bnt.addEventListener("click",function() {
+        if(input.innerHTML=='0')
+        input .innerHTML='';
+        if(bnt.innerHTML=='AC')
+        input.innerHTML='0';
+        else if(bnt.innerHTML=='DEL')   {
+            var arrtext = Array.from(input.innerHTML);
+            arrtext.splice(arrtex.length-1,1);
+            if(arrtex.length!=0)
+            input.innerHTML=arrtext.join('');
+            else input.innerHTML='0';
+        }
+        else if(bnt.innerHTML=='=') {
+            input.innerHTML = eval(input.innerHTML);
+        }
+        else input.innerHTML+=bnt.innerHTML;
+    }) 
+}) 
